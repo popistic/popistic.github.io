88ae23c270943c9d678d24b79d8c3a82d3b597ac
popistic <teampopistic@gmail.com>
2023-10-09 19:05:52 +0530

    Changes to sync products

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-/*  ---------------------------------------------------
-Template Name: Ashion
-Description: Ashion ecommerce template
-Author: Colorib
-Author URI: https://colorlib.com/
-Version: 1.0
-Created: Colorib
----------------------------------------------------------  */
-
 'use strict';
 
 (function ($) {
@@ -16,27 +7,14 @@ Created: Colorib
     --------------------*/
     $(window).on('load', function () {
         $(".loader").fadeOut();
-        $("#preloder").delay(500).fadeOut("slow");
+        $("#preloder").delay(750).fadeOut("slow");       
 
-        /*------------------
-            Product filter
-        --------------------*/
-        $('.filter__controls li').on('click', function () {
-            $('.filter__controls li').removeClass('active');
-            $(this).addClass('active');
+        $.ajax({
+            url: "https://capi.mini.store/api/v1/stores/popistic/catalog",
+            success: function(data){
+                process_response(data);
+            }
         });
-        if ($('.property__gallery').length > 0) {
-            var containerEl = document.querySelector('.property__gallery');
-            var mixer = mixitup(containerEl);
-        }
-    });
-
-    /*------------------
-        Background Set
-    --------------------*/
-    $('.set-bg').each(function () {
-        var bg = $(this).data('setbg');
-        $(this).css('background-image', 'url(' + bg + ')');
     });
 
     //Search Switch
@@ -121,6 +99,65 @@ Created: Colorib
                 e.classList.add("active");
             }
         })
+    }
+
+    function process_response(data){
+        // product categories
+        var category_list = {};
+        for (var category of data.data.categories){
+            category_list[category.id] = category.name
+            if (category.name != "other"){
+                $(".filter__controls").append(`<li data-filter=".${category.name.replace(/ /g,"").toLowerCase()}">${category.name}</li>`);
+            }
+        }
+
+        for (var product of data.data.products){
+            if (product.active){
+                console.log(product)
+                console.log(category_list[product.categories[0]].replace(/ /g,"-"));
+
+                // Populate products
+                $(".property__gallery").append(`<div class="col-lg-3 col-md-4 col-6 mix ${category_list[product.categories[0]].replace(/ /g,"").toLowerCase()}">
+                <div class="product__item">
+                    <div class="product__item__pic set-bg" data-setbg="https://minis-media-assets.swiggy.com/swiggymini/image/upload/w_500,h_500,c_fit,q_auto,f_auto/${product.images[0]}">
+                        ${product.badges.length>0 ? `<div class="label new">${product.badges[0]}</div>` : ''}
+                        <ul class="product__hover">
+                            <li><a target="_blank" href="https://popistic.mini.store/products/${product.id}"><i class="fa-solid fa-bag-shopping"></i></a></li>
+                        </ul>
+                    </div>
+                    <div class="product__item__text">
+                        <h6><a class="text-decoration-none" target="_blank" href="https://popistic.mini.store/products/${product.id}">${product.name}</a></h6>
+
+                        ${product.discountPercent ? `<div class="product__price">₹ ${product.discountedPrice}</div> <span class="text-danger">(${product.discountPercent}% off)</span>` : '<div class="product__price">₹ ${product.price}</div>'}
+
+                        ${product.discountPercent ? `<div class="product__price text-muted"><del>₹ ${product.price}</del></div>` : ''}
+                    </div>
+                </div>
+            </div>`)
+            }
+        }
+
+        /*------------------
+        Background Set
+        --------------------*/
+        $('.set-bg').each(function () {
+            var bg = $(this).data('setbg');
+            $(this).css('background-image', 'url(' + bg + ')');
+        });
+
+
+                /*------------------
+        Product filter
+        --------------------*/
+        $('.filter__controls li').on('click', function () {
+            $('.filter__controls li').removeClass('active');
+            $(this).addClass('active');
+        });
+        if ($('.property__gallery').length > 0) {
+            var containerEl = document.querySelector('.property__gallery');
+            var mixer = mixitup(containerEl);
+        }
+
     }
 
 
